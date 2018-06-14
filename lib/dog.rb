5dc49305a0ba0516770548b38ae99251f564c9de
Priscilla H <priscillamhuynh@gmail.com>
2018-06-14 08:45:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,11 +12,11 @@ class Dog
     @name
   end
 
-  def breed=(breed)
-    @breed=breed
-  end
-
-  def breed
-    @breed
-  end
+  # def breed=(breed)
+  #   @breed=breed
+  # end
+  # 
+  # def breed
+  #   @breed
+  # end
 end
